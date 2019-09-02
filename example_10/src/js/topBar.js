822ce6aa9b41430d121b6ba2a0d77e687176ce56
Andrew Pantuso <ajpantuso@gmail.com>
2019-09-01 20:32:57 -0400

    example 10 enhancements including actual content.

--- example_10/src/js/topBar.js
+++ example_10/src/js/topBar.js
@@ -1,5 +1,6 @@
 import {MDCTopAppBar} from "@material/top-app-bar";
 import {MDCDrawer} from "@material/drawer";
+import {MDCTextField} from '@material/textfield';
 
 const topAppBar = MDCTopAppBar.attachTo(document.getElementById('app-bar')); // eslint-disable-line no-undef
 const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer')); // eslint-disable-line no-undef
@@ -8,3 +9,30 @@ topAppBar.setScrollTarget(document.getElementById('main-content')); // eslint-di
 topAppBar.listen('MDCTopAppBar:nav', () => {
   drawer.open = !drawer.open;
 });
+
+const edit = document.getElementById('edit');
+
+edit.addEventListener('click', () => {
+  var template = require("../views/textArea.pug");
+  var mainTextField = document.getElementById('main-text-field');
+  if (! mainTextField) {
+    var mainContent = document.getElementById('main-content');
+    mainContent.innerHTML = template({});
+    const textField = MDCTextField.attachTo(document.querySelector('.mdc-text-field')); // eslint-disable-line no-undef
+  }
+})
+
+const clear = document.getElementById('clear');
+
+clear.addEventListener('click', () => {
+  const textField = MDCTextField.attachTo(document.querySelector('.mdc-text-field')); // eslint-disable-line no-undef
+  textField.value = "";
+})
+
+//Save will store the thoughts in a list to be displayed with another button
+const save = document.getElementById('save');
+
+save.addEventListener('click', () => {
+  const content = document.getElementById('main-content');
+  content.innerHTML = "";
+})
